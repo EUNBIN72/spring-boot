45fe0cf191f3e0f240e75f7af946e4b727e095c5
eunbin <kozilee00@gmail.com>
2025-08-05 17:04:58 +0900

    GDJ_92_Maven

--- GDJ_92_Maven/src/test/java/com/winter/app/board/notice/NoticeDAOTest.java
+++ GDJ_92_Maven/src/test/java/com/winter/app/board/notice/NoticeDAOTest.java
@@ -25,41 +25,51 @@ class NoticeDAOTest {
 		assertNotEquals(0, list);
 	}
 
-	@Test // test를 할 수 있는 어노테이션
-	void detailTest() throws Exception {
-		NoticeVO noticeVO = new NoticeVO();
-		noticeVO.setBoardNum(1L); // long 타입으로 형 변환 해야 됨
-		BoardVO boardVO = noticeDAO.detail(noticeVO);
-		log.info("result : {}", boardVO);
-
-		// 단정문
-		assertNotNull(boardVO);
-	}
+//	@Test // test를 할 수 있는 어노테이션
+//	void detailTest() throws Exception {
+//		NoticeVO noticeVO = new NoticeVO();
+//		noticeVO.setBoardNum(1L); // long 타입으로 형 변환 해야 됨
+//		BoardVO boardVO = noticeDAO.detail(noticeVO);
+//		log.info("result : {}", boardVO);
+//
+//		// 단정문
+//		assertNotNull(boardVO);
+//	}
 
 	
-	  @Test void insertTest() throws Exception { NoticeVO noticeVO = new
-	  NoticeVO(); noticeVO.setBoardTitle("title3");
-	  noticeVO.setBoardContents("contents3"); noticeVO.setBoardWriter("writer3");
-	  int result = noticeDAO.insert(noticeVO);
-	  
-	  // 단정문 assertEquals(1, result);
+	  @Test 
+	  void insertTest() throws Exception { 
+		  
+		  for(int i=0; i<105; i++) {
+		
+		  NoticeVO noticeVO = new  NoticeVO(); 
+		  noticeVO.setBoardTitle("title3" + i);
+		  noticeVO.setBoardContents("contents3" + i); 
+		  noticeVO.setBoardWriter("writer3" + i);
+		  int result = noticeDAO.insert(noticeVO);
+		  if(i%10 == 0) {
+			  // 너무 많이 한번에 넣으면 멈춰버리니까 잠깐 쉬었다가 진행시키기 위한 코드
+			  Thread.sleep(500);
+		  }
+		   } 
+		  // 단정문 assertEquals(1, result);
 	  
 	  }
 	  
-	  @Test void updateTest() throws Exception { NoticeVO noticeVO = new
-	  NoticeVO(); noticeVO.setBoardTitle("update_title");
-	  noticeVO.setBoardContents("update_contents");
-	  noticeVO.setBoardWriter("update_writer"); int result =
-	  noticeDAO.update(noticeVO);
-	  
-	  assertEquals(1, result); }
-	  
-	  @Test void deleteTest() throws Exception { NoticeVO noticeVO = new
-	  NoticeVO(); noticeVO.setBoardTitle("update_title");
-	  noticeVO.setBoardContents("update_contents");
-	  noticeVO.setBoardWriter("update_writer"); int result = noticeDAO.delete(noticeVO);
-	  
-	  assertEquals(1, result); }
-	  
+//	  @Test void updateTest() throws Exception { NoticeVO noticeVO = new
+//	  NoticeVO(); noticeVO.setBoardTitle("update_title");
+//	  noticeVO.setBoardContents("update_contents");
+//	  noticeVO.setBoardWriter("update_writer"); int result =
+//	  noticeDAO.update(noticeVO);
+//	  
+//	  assertEquals(1, result); }
+//	  
+//	  @Test void deleteTest() throws Exception { NoticeVO noticeVO = new
+//	  NoticeVO(); noticeVO.setBoardTitle("update_title");
+//	  noticeVO.setBoardContents("update_contents");
+//	  noticeVO.setBoardWriter("update_writer"); int result = noticeDAO.delete(noticeVO);
+//	  
+//	  assertEquals(1, result); }
+//	  
 
 }
