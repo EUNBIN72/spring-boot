4f5b92febfad5e01b0c72c03e0f90c99d2fa90ed
eunbin <kozilee00@gmail.com>
2025-08-28 16:14:23 +0900

    Security

--- GDJ_92_Maven/src/main/java/com/winter/app/configs/security/AddLogoutHandler.java
+++ GDJ_92_Maven/src/main/java/com/winter/app/configs/security/AddLogoutHandler.java
@@ -14,10 +14,16 @@ public class AddLogoutHandler implements LogoutHandler {
 // LogoutHandler : Spring Security 가 제공하는 로그아웃 처리용 인터페이스
 
 	@Override
+	// HttpServletRequest request : 로그아웃 요청 정보
+	// HttpServletResponse response : 응답 객체. (쿠키 삭제, 헤더 추가 가능)
+	// Authentication authentication : 로그아웃하는 사용자의 인증 정보 (로그인 아이디, 권한 등)
 	public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
 		log.info("logout handler");
 		log.info("{}", authentication);
 
 	}
+	
+	// 사용자가 /member/logout 같은 로그아웃 URL을 호출하면 Spring Security의 LogoutFilter -> SecurityContextLogoutHandler가 동작
+	
 
 }
